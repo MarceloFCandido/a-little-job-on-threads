a1236d773edd7bc0282eefd06bee6c9dd0d5aef3
rubiotorres <rubiotorres15@gmail.com>
2019-04-03 17:32:34 -0300

    Implemented reading results waiting further tsting

--- src/array-and-pthread.c
+++ src/array-and-pthread.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <pthread.h>
 
-#define ARRAY_SIZE 1024 * 1024
+#define ARRAY_SIZE 10
 #define NUM_THREADS 4
 
 typedef struct kit_t {
@@ -27,12 +27,33 @@ void *array_build(void *kit) {
 }
 
 void *array_greatest(void *kit) {
+    // Casting the kit
     KIT_t *kit_casted = (KIT_t *) kit;
+
+    int thread_id = kit_casted->thread_id;
+    int *array = kit_casted->array;
+    int *result;
+    int num_positions = ARRAY_SIZE / NUM_THREADS;
+   
+    result=malloc(sizeof(int));
+    *result=0;
+    
+    for (int i = thread_id * num_positions;
+                                    i < (thread_id + 1) * num_positions; i++){
+        if(array[i]>*result){
+            *result=array[i];
+        }
+    }
+    
+    pthread_exit(result);
 }
 
 int main(int argc, char const *argv[]) {
 
     srand(time(0));
+
+    void *thread_return;
+    int result=0;
 
     // Creating a int array of 4 MB
     int *array = (int *) malloc(ARRAY_SIZE * sizeof(int));
@@ -67,28 +88,27 @@ int main(int argc, char const *argv[]) {
             exit(-1);
         }
     }
-
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("%05d\n", array[i]);
-    }
-
-    // Creating threads for searching the greatest element on the array
-    // for (int i = 0; i < NUM_THREADS; i++) {
-    //     if (pthread_create(&threads[i], &attr, array_greatest, (void *) kit)) {
-    //         // error
-    //         printf("Error creating thread %d", i);
-    //         exit(-1);
-    //     }
-    // }
-    //
-    // // Joining threads
-    // for (int i = 0; i < NUM_THREADS; i++) {
-    //     if (pthread_join(threads[i], NULL)) {
-    //         // error
-    //         printf("Error joining thread %d", i);
-    //         exit(-1);
-    //     }
-    // }
+    //Creating threads for searching the greatest element on the array
+        for (int i = 0; i < NUM_THREADS; i++) {
+        if (pthread_create(&threads[i], &attr, array_greatest, (void *) &kit[i])) {
+            // error
+            printf("Error creating thread %d", i);
+            exit(-1);
+        }
+     }
+    
+    // Joining threads
+    for (int i = 0; i < NUM_THREADS; i++) {
+        if (pthread_join(threads[i], &thread_return)) {
+            // error
+            printf("Error joining thread %d", i);
+            exit(-1);
+        }
+        if(result<*(int*)thread_return){
+            result=*(int*)thread_return;
+        }
+     }
+     printf("The final result is %d.\n", result);
 
 
 
